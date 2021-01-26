2abd581388c218cd93f81bf35b622d0ab7ca83e1
JCahill <cahilljm@aol.com>
2021-01-25 18:14:01 -0800

    Lesson 02 Javascript Assignment J Cahill

---
+++ lesson-02/calculator.js
@@ -0,0 +1,19 @@
+let v = 10;
+let w = 2;
+let x = 20
+let y = 5;
+
+//addition
+console.log(v + w + x + y)
+
+//subtraction
+console.log(x - v - w - y)
+
+//multiplication
+console.log(((x * v) * w)* y)
+
+//division
+console.log(((x / v) / w) / y)
+
+//combination
+console.log((((x * v) / y) + w) - y)
