fcb8c1f415abb1014690ff770ccbf162af643bb9
JCahill <cahilljm@aol.com>
2021-02-01 09:34:36 -0800

    Update less3-ex4 and less3-ex3

--- lesson-03/Lesson03-exercise-04-bonus.js
+++ lesson-03/Lesson03-exercise-04-bonus.js
@@ -1,7 +1,7 @@
 let arr = [['Clubs', 'Diamonds', 'Hearts', 'Spades'], ['ace', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'jack', 'queen', 'king']];
-console.log(arr)
- 
+console.log(arr);
+
 for (let i = 0; i < arr[0].length; i++) {
-forEach (let j = 0; j < arr[1].length; j++)
- console.log(arr[1][j] + ' of ' + arr[0][i])
- }
+  for (let j = 0; j < arr[1].length; j++)
+  console.log(arr[1][j] + ' of ' + arr[0][i] );
+}
